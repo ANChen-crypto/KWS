cf9d6f70c1a214bb83d6a3ad8beb920dbce2e7cf
Deepfens-AN <za.chen066@gmail.com>
2025-12-06 16:48:50 +0800

    kws environment build up 1206

--- main.py
+++ main.py
@@ -16,12 +16,9 @@ def main(args):
 
 if __name__ == "__main__":
 
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("--train", action="store_true")
-    # parser.add_argument("--ckpt")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--ckpt")
 
-    # args = parser.parse_args()
-    # main(args)
-    model = KWSModel()
-    trainer = Trainer(model)
-    trainer.train()
+    args = parser.parse_args()
+    main(args)
